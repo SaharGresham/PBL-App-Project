3c53d056dfa5482a3c373b9dbfb6c3d3a99c4682
Sahar Gresham <sahargresham.gmail.com>
2020-05-21 19:56:14 -0600

    Second commit with files

---
+++ src/scripts/pblApp.js
@@ -0,0 +1,8 @@
+
+import fetchfunctions from './fetch-pblApp.js';
+const apblFunction={
+    play(){
+        console.log(" WE ARE A GREAT TEAM");
+    },
+}
+export default apblFunction;
---
+++ src/scripts/pblApp.test.js
@@ -0,0 +1,7 @@
+//npm test -- -t pblApp
+import fetchfunctions from './fetch-pblApp.js'
+import stuff from './pblApp.js';
+test ("test the plumbing", () =>{
+    console.log("This will be a great project");
+    stuff.play();
+});
